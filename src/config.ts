fe05ad412dd663150baf4d51e048ec1a6e1aa83e
richslezak <richslezak@gmail.com>
2023-12-29 18:38:00 -0500

    site build

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Rich Slezak';
+export const SITE_DESCRIPTION = 'Rich Slezak is a dude who does stuff.';
+export const SITE_KEYWORDS = 'Richard Slezak, Moutain Biker, BMX Rider, Web Developer';
+export const SITE_AUTHOR = 'Richard Slezak';
+export const SITE_URL = 'https://richslezak.com';
