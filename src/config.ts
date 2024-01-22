f656ee77c89f676eca8354b618568597fe7d9732
richslezak <richslezak@gmail.com>
2024-01-22 13:33:08 -0500

    update to meta info

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Rich Slezak';
-export const SITE_DESCRIPTION = 'Rich Slezak is a dude who does stuff.';
-export const SITE_KEYWORDS = 'Richard Slezak, Moutain Biker, BMX Rider, Web Developer';
+export const SITE_DESCRIPTION =
+  'Rich Slezak, a digital designer and developer based in Boston, MA. This site is a placehodler for richslezak, richardslezak, richardjslezak and rjslezak domain names. For digital portfolio please visit tentoden.com';
+export const SITE_KEYWORDS =
+  'Richard Slezak, Richard J. Slezak, Rich Slezak, Moutain Biker, BMX Rider, Cyclist, Digital Artist, Guitar Technician, Web Developer, Visual Artist, Graphic Designer, Tentoden, Tentoden.com';
 export const SITE_AUTHOR = 'Richard Slezak';
 export const SITE_URL = 'https://richslezak.com';
